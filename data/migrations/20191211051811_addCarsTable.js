92932728c5a06a82e584777fd67ff9cb851f7791
DeAndre Johnson <dre@phreshr.com>
2019-12-11 13:36:23 +0800

    update schema

--- data/migrations/20191211051811_addCarsTable.js
+++ data/migrations/20191211051811_addCarsTable.js
@@ -3,12 +3,24 @@ exports.up = function(knex) {
     table.increments();
     table.string("make", 255).notNullable();
     table.string("model", 255).notNullable();
-    table.string("year", 255).notNullable();
+    table.integer("mileage", 255).notNullable();
+    table.integer("year", 4).notNullable();
     table
       .string("vin", 255)
       .notNullable()
       .unique();
-    table.boolean("is_used", 255).defaultTo(false);
+    table
+      .boolean("is_used")
+      .notNullable()
+      .defaultTo(false);
+    table
+      .enu("transmission", ["v6", "v8", "v12"])
+      .notNullable()
+      .defaultTo("v6");
+    table
+      .enu("title-status", ["Clean", "Salvage", "Junk", "Bonded"])
+      .notNullable()
+      .defaultTo("Clean");
     table.float("price", 255).notNullable();
     table.timestamps(false, true);
   });
